bd7b237b6d12469f5f4a8299ade0de74ca878d7f
William Loughney <woughney1@gmail.com>
2021-11-09 15:33:04 +0000

    define 'area' method on Rectangle struct

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,21 @@ struct Rectangle {
     height: u32,
 }
 
+// We can define methods on structs like so. 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
-    dbg!(&rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
