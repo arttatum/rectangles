85a153cf7e2e1768ab4d057cb177b831bc7d10dd
William Loughney <woughney1@gmail.com>
2021-11-09 15:07:56 +0000

    use dbg!() to print debugging info to console

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,11 @@ struct Rectangle {
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-
-    // we need our struct to have the attribute #[derive(Debug)] to use these notations
-    println!("rect1 is {:?}", rect1);
-    println!("rect1 is {:#?}", rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    dbg!(&rect1);
 }
